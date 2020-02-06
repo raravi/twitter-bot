3313db83d180d61055c005e0fb017933624553e4
Amith Raravi <amith.raravi@gmail.com>
2020-02-06 19:36:34 +0100

    Refactored Javascript code into IIFE for better maintainability.

--- bot.js
+++ bot.js
@@ -3,105 +3,208 @@ const Sentiment = require('sentiment');
 const vader = require('vader-sentiment');
 const twit = require('twit');
 const dotenv = require('dotenv');
-
-const interval = 6 * 60 * 60 * 1000;
-
 dotenv.config();
 
-const Twitter = new twit({
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
+(function(root) {
+  function TwitterBot() {
+    this.unitOfTime = 'hours';
+    this.oneUnitOfTime = 60 * 60 * 1000;
+    this.numberOfUnitsOfTimeForRetweet = 6;
+    this.numberOfUnitsOfTimeForFollow = 6;
+    this.intervalForRetweet = this.numberOfUnitsOfTimeForRetweet * this.oneUnitOfTime;
+    this.intervalForFollow = this.numberOfUnitsOfTimeForFollow * this.oneUnitOfTime;
+    this.selectedTweetForRetweet;
+    this.selectedTweetForFollow;
 
-console.log('The bot has started...');
+    this.Twitter = new twit({
+      consumer_key: process.env.BOT_CONSUMER_KEY,
+      consumer_secret: process.env.BOT_CONSUMER_SECRET,
+      access_token: process.env.BOT_ACCESS_TOKEN,
+      access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+    });
 
-function retweet() {
-  let params = {
-    q: '#100DaysOfCode -filter:retweets',
-    result_type: 'recent',
-    count: 50,
-    lang: 'en'
-  };
-  let optionsForSentiment = {
-    extras: {
-      'learned': 2,
-      'learnt': 2,
-      'learning': 2,
-      'learn': 1
-    }
-  };
-  let sentiment = new Sentiment();
+    this.paramsForSearch = {
+      q: '#100DaysOfCode -filter:retweets',
+      result_type: 'recent',
+      count: 50,
+      lang: 'en'
+    };
 
-  Twitter.get('search/tweets', params, (err, data, response) => {
-    if (err) {
-       console.log(err);
-    }
-    else {
-      let tweets = [];
-      // console.log('-------------------------------');
-      // console.log('Tweets returned: ', data.statuses.length);
-      // console.log('-------------------------------');
-      for (let i = 0; i < data.statuses.length; i++) {
-        let tweet = data.statuses[i];
-        let timeElapsed;
-        try {
-          timeElapsed = moment().diff(moment(tweet.created_at), 'hours');
-        } catch(e) {
-          //console.err('Created Time not picked up by Moment.js!');
-          timeElapsed = null;
-        }
-        if (tweet.retweeted_status === undefined &&
-            Number.isInteger(timeElapsed) && timeElapsed < 6 &&
-            !tweet.possibly_sensitive
-          ) {
-          let result = sentiment.analyze(tweet.text, optionsForSentiment);
-          const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(tweet.text);
-          // console.log(timeElapsed, ' hour(s) ago');
-          // console.log('Sentiment: ', result.score);
-          // console.log('Vader Sentiment: ', intensity);
-          // console.log(tweet.text);
-          // console.log(tweet.possibly_sensitive);
-          // console.log('-------------------------------');
-          if (intensity.compound > 0.2 )
-            tweets.push(tweet);
-        }
+    this.optionsForSentiment = {
+      extras: {
+        'learned': 2,
+        'learnt': 2,
+        'learning': 2,
+        'learn': 1
       }
-      //console.log('Length: ', tweets.length);
-      if (tweets.length === 0)
-        return true;
-      let selectedTweet = tweets[Math.random() * tweets.length | 0];
-      //console.log(selectedTweet);
-      Twitter.post( 'statuses/retweet/:id',
-                    { id: selectedTweet.id_str },
-                    (err, data, response) => {
-                      if (err) {
-                         console.log(err);
-                      }
-                      else {
-                       console.log('Retweeted! ');
-                      }
-                    });
-    }
-  });
-}
+    };
 
-function tweet(text) {
-  let tweet = {
-      status: text
+    this.sentiment = new Sentiment();
+  }
+
+  TwitterBot.prototype.tweet = function (text) {
+    let tweet = {
+        status: text
+    };
+
+    this.Twitter.post('statuses/update', tweet, (err, data, response) => {
+      if (err) {
+         console.log(err);
+      }
+      else {
+       console.log('Tweet Posted!');
+      }
+    });
   };
 
-  Twitter.post('statuses/update', tweet, (err, data, response) => {
-    if (err) {
-       console.log(err);
-    }
-    else {
-     console.log('Tweet Posted!');
-    }
-  });
-}
+  TwitterBot.prototype.retweet = function() {
+    this.Twitter.get('search/tweets', this.paramsForSearch, (err, data, response) => {
+      if (err) {
+         console.log(err);
+      }
+      else {
+        let tweets = [];
+        console.log('-------------------------------');
+        console.log('Tweets returned: ', data.statuses.length);
+        console.log('-------------------------------');
+        for (let i = 0; i < data.statuses.length; i++) {
+          let tweet = data.statuses[i];
+          let timeElapsed;
+          try {
+            timeElapsed = moment().diff(moment(tweet.created_at), this.unitOfTime);
+          } catch(e) {
+            console.err('Created Time not picked up by Moment.js!');
+            timeElapsed = null;
+          }
+          console.log('tweet.retweeted_status: ', tweet.retweeted_status);
+          console.log('tweet.possibly_sensitive: ', tweet.possibly_sensitive);
+          if (tweet.retweeted_status === undefined &&
+              Number.isInteger(timeElapsed) && timeElapsed < this.numberOfUnitsOfTimeForRetweet &&
+              !tweet.possibly_sensitive
+            ) {
+            let result = this.sentiment.analyze(tweet.text, this.optionsForSentiment);
+            const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(tweet.text);
+            // console.log(timeElapsed, ' hour(s) ago');
+            // console.log('Sentiment: ', result.score);
+            console.log('Vader Sentiment: ', intensity);
+            // console.log(tweet.text);
+            if (intensity.compound > 0.2 )
+              tweets.push(tweet);
+          }
+          console.log('-------------------------------');
+        }
+        console.log('Length: ', tweets.length);
+        if (tweets.length === 0)
+          return true;
+        this.selectedTweetForRetweet = tweets[Math.random() * tweets.length | 0];
+        // console.log(this.selectedTweetForRetweet);
+        this.Twitter.post( 'statuses/retweet/:id',
+                      { id: this.selectedTweetForRetweet.id_str },
+                      (err, data, response) => {
+                        if (err) {
+                           console.log(err);
+                        }
+                        else {
+                          console.log('Retweeted: ', this.selectedTweetForRetweet.id_str);
+                        }
+                      });
+      }
+    });
+  };
 
-retweet(); // Runs on first load
-setInterval(retweet, interval); // Retweets every 6 hours
-//tweet('Hello World');
+  TwitterBot.prototype.follow = function () {
+    this.Twitter.get('friends/ids', { screen_name: 'amith_raravi' , stringify_ids: true }, (err, data, response) => {
+      if (err) {
+         console.log(err);
+      }
+      else {
+       let friends = data.ids;
+       this.Twitter.get('search/tweets', this.paramsForSearch, (err, data, response) => {
+         if (err) {
+            console.log(err);
+         }
+         else {
+           let tweets = [];
+           console.log('-------------------------------');
+           console.log('Tweets returned: ', data.statuses.length);
+           console.log('-------------------------------');
+           for (let i = 0; i < data.statuses.length; i++) {
+             let tweet = data.statuses[i];
+             let timeElapsed;
+             try {
+               timeElapsed = moment().diff(moment(tweet.created_at), this.unitOfTime);
+             } catch(e) {
+               console.err('Created Time not picked up by Moment.js!');
+               timeElapsed = null;
+             }
+             console.log('tweet.retweeted_status: ', tweet.retweeted_status);
+             console.log('tweet.possibly_sensitive: ', tweet.possibly_sensitive);
+             if (tweet.retweeted_status === undefined &&
+                 friends.includes(tweet.user.id_str) === false &&
+                 Number.isInteger(timeElapsed) && timeElapsed < this.numberOfUnitsOfTimeForFollow &&
+                 !tweet.possibly_sensitive
+               ) {
+               let result = this.sentiment.analyze(tweet.text, this.optionsForSentiment);
+               const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(tweet.text);
+               // console.log(timeElapsed, ' hour(s) ago');
+               // console.log('Sentiment: ', result.score);
+               console.log('Vader Sentiment: ', intensity);
+               console.log('tweet.user.id_str: ', tweet.user.id_str);
+               // console.log(tweet.text);
+               if (intensity.compound > 0.2 )
+                 tweets.push(tweet);
+             }
+             console.log('-------------------------------');
+           }
+           console.log('Length: ', tweets.length);
+           if (tweets.length === 0)
+             return true;
+           this.selectedTweetForFollow = tweets[Math.random() * tweets.length | 0];
+           // console.log(this.selectedTweetForFollow);
+           this.Twitter.post('friendships/create', {user_id: this.selectedTweetForFollow.user.id_str}, (err, res) => {
+             if(err){
+               console.log(err);
+             } else {
+               console.log('Followed: @', this.selectedTweetForFollow.user.screen_name);
+             }
+           });
+         }
+       });
+      }
+    });
+  };
+
+  TwitterBot.prototype.greetNewFollowers = function() {
+    var stream = Twitter.stream('user');
+    stream.on('follow', (event) => {
+      console.log('Follow Event is running');
+      var
+        name = event.source.name,
+        screenName = event.source.screen_name;
+      var tweetTxt = '@' + screenName + ' Thank you for following me!';
+      var tweet = {
+          status: tweetTxt
+      };
+      Twitter.post('statuses/update', tweet, (err, data, response) => {
+        if(err){
+          console.log("Error in Replying");
+        }
+        else{
+          console.log("Gratitude shown successfully");
+        }
+      });
+    });
+  };
+
+  let twitterBot = new TwitterBot();
+  root.twitterBot = twitterBot;
+
+  console.log('The bot has started...');
+
+  // twitterBot.tweet("\“Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.\”\n\n― Rick Cook, The Wizardry Compiled\n\n#codinghumor");
+  twitterBot.retweet();
+  setInterval(twitterBot.retweet, this.intervalForRetweet);
+  twitterBot.follow();
+  setInterval(twitterBot.follow, this.intervalForFollow);
+  // twitterBot.greetNewFollowers();
+})(this);
